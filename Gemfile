a3857b2e275e7df32ffb210e2f0eea20170c0961
Nadia Salmen <nadiasalmen@gmail.com>
2021-03-02 23:42:21 -0300

    Create seeds to launch the app

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby '2.6.6'
 # Custom gems
 # authorization
 gem 'pundit'
+# fake seeds
+gem 'faker'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.5'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+PAYMENT_STATUSES = ['pagada', 'no pagada', 'pendiente de pago']
+ORDER_STATUSES = ['recibida', 'en preparación', 'en reparto', 'entregada']
+
+puts 'Cleaning database...'
+Order.destroy_all
+User.destroy_all
+
+puts 'Creating new users'
+
+5.times do
+  user = User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+  puts "Creating unpaid and undelivered orders for user #{user.id}"
+  5.times do
+    order = Order.new(
+      amount: rand(1000..5000),
+      payment_status: ['no pagada', 'pendiente de pago'].sample,
+      order_status: ['recibida', 'en preparación', 'en reparto'].sample
+    )
+    order.user = user
+    order.save!
+  end
+  puts "Creating paid and undelivered orders for user #{user.id}"
+  3.times do
+    order = Order.new(
+      amount: rand(1000..5000),
+      payment_status: 'pagada',
+      payment_date: Date.today - rand(0..5),
+      order_status: ['recibida', 'en preparación', 'en reparto'].sample
+    )
+    order.user = user
+    order.save!
+  end
+  puts "Creating unpaid and delivered orders for user #{user.id}"
+  3.times do
+    order = Order.new(
+      amount: rand(1000..5000),
+      payment_status: ['no pagada', 'pendiente de pago'].sample,
+      order_status: 'entregada',
+      delivery_date: Date.today - rand(0..5)
+    )
+    order.user = user
+    order.save!
+  end
+end
