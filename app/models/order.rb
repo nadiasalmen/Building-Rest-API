f9cfc18d7145db960e66f3dc1de7ece9115f0d58
Nadia Salmen <nadiasalmen@gmail.com>
2021-03-02 22:59:11 -0300

    add model associations and validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,22 @@
 class Order < ApplicationRecord
   belongs_to :user
+
+  validates :amount, presence: true
+  validates :amount, numericality: true
+  validates :payment_status, presence: true
+  validates :payment_status, inclusion: { in: ['pagada', 'no pagada', 'pendiente de pago']}
+  validates :order_status, presence: true
+  validates :order_status, inclusion: { in: ['recibida', 'en preparación', 'en reparto', 'entregada']}
+  validate :add_payment_date
+  validate :add_delivery_date
+
+  private
+
+  def add_payment_date
+    errors.add('Debe agregar la fecha de pago') if payment_status == 'pagada' || payment_date.nil?
+  end
+
+  def add_delivery_date
+    errors.add('Debe agregar la fecha de entrega') if order_status == 'entregada' || delivery_date.nil?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :orders
+
+  validates :name, presence: true
+  validates :name, length: { in: 2..50 }
+  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
